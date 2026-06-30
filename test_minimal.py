5b824bf0c0fe692f068adaa5c549307d275a2460
Your Name <you@example.com>
2026-07-01 01:55:48 +0800

    Remove test files

--- test_minimal.py
+++
@@ -1,6 +0,0 @@
-﻿import sys, os, webview
-VERSION = "1.1.0"
-def start():
-    webview.create_window("AniFlow Test", html="<h1>Hello</h1><p>If you see this, WebView2 works!</p>", width=400, height=300)
-if __name__ == "__main__":
-    start()
